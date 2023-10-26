13e4defd1475952222e928c7af049a696a14d97a
persius21 <145455839+vi2uba@users.noreply.github.com>
2023-10-26 12:19:47 +0800

    October 26 2023 12:19PM
    -added nodemon for quicker reload
    -added morgan for loggin
    -added error handling

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const barangayRoutes = require('./API/routes/barangay');
 const peopleRoutes = require('./API/routes/people');
 
+//Morgan Logs Requests
+app.use(morgan('dev'));
+
+//Routes that will handle the requestsnp
 app.use('/barangay', barangayRoutes);
 app.use('/people', peopleRoutes);
 
+//Error Handler
+app.use((req,res,next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+
+})
+app.use((error,req,res,next) => {
+    res.status(error.status||500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+
+})
+
+//runs app
 module.exports = app;
